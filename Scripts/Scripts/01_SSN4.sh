54fbb5080810d9b2f0d12ec05e62f74a04e37f1b
Vincent Koppelmans <vincent.koppelmans@utah.edu>
2019-06-26 09:56:20 +0200

    Path updates

--- Scripts/Scripts/01_SSN4.sh
+++ Scripts/Scripts/01_SSN4.sh
@@ -1,92 +1,138 @@
 #!/bin/bash
 
-# This script performas N4 bias field correction within a brain mask.
+# This script performs N4 bias field correction within a brain mask.
 
 # Environment
-iDIR=/input/sub-${SID}
-oDIR=/output/derivatives
-
-
-
-
-lhab_data="/home/vkoppe-utah/lhab_data/LHAB/LHAB_v1.1.1"
-iDIR="${lhab_data}/sourcedata"
-oDIR="${lhab_data}/derivatives/CerebSUIT_20190617"
-tDIR="/path/to/MICCAI2012-Multi-Atlas-Challenge-Data"
+SID="${1}"
+SES="${2}"
+iDIR=/input/ses-${SES}/anat
+oDIR=/output/sub-${SID}/ses-${SES}/01_SSN4
 mkdir -p ${oDIR}
-
-# Loop over subjects
-for SUB in $(ls ${iDIR}); do
-
-    # Create Script for N4 bias field correction
-
-	#!/bin/bash
-
-	# This script creates a skull stripped brain
-	# image for subject ${SUB}. It was generated
-	# on $(date).
-
-	# Environment
-	SUB=${SUB}
-	iDIR=${iDIR}/\${SUB}
-	oDIR=${oDIR}/\${SUB}/01_SSN4
-	mkdir -p \${oDIR}	
-
-	# Copy over data
-	
+tDIR="/sofware/ANTS-templates"
 
 
-	# Skull Strip
-	echo "<---------- 01: Skull Strip ---------->"
-	antsBrainExtraction.sh \
-	    -d 3 \
-	    -a ${iDIR}/memprage.nii \
-	    -e ${tDIR}/T_template0.nii.gz \
-	    -m ${tDIR}/T_template0_BrainCerebellumProbabilityMask.nii.gz \
-	    -f ${tDIR}/T_template0_BrainCerebellumRegistrationMask.nii.gz \
-	    -o ${oDIR}/ss_${SUB}_ \
-	    -k 0
-	
-	
-	# N4 Bias Field Correction
-	echo "<---------- 02: N4 Bias Field Correction ---------->"
-	fslmaths \
-	    ${oDIR}/ss_${SUB}_BrainExtractionMask.nii.gz \
-	    -dilM -dilM \
-	    ${oDIR}/ss_${SUB}_BrainExtractionMask_dilM2.nii.gz
-	
-	# ITK is very sensitive with any deviations between
-	# sform / qform codes. Decimal changes that occur
-	# will result in problems with the bias field correction.
-	# We are therefore going to copy over the affine matrix
-	# to avoid this.
-	fslcpgeom \
-	    ${iDIR}/memprage.nii \
-	    ${oDIR}/ss_${SUB}_BrainExtractionMask.nii.gz
-	
-	fslcpgeom \
-	    ${iDIR}/memprage.nii \
-	    ${oDIR}/ss_${SUB}_BrainExtractionMask_dilM2.nii.gz
-	
-	# Note: adding a mask forces the N4 application within the mask
-	# We want to do the estimation within the mask, but the application
-	# on the whole image. So we use -w but not -x.
-	N4BiasFieldCorrection \
-	    -d 3 \
-	    -i ${iDIR}/memprage.nii \
-	    -w ${oDIR}/ss_${SUB}_BrainExtractionMask_dilM2.nii.gz \
-	    -s 2 \
-	    -c [125x100x75x50] \
-	    -o [${oDIR}/N4_${SUB}.nii.gz,${oDIR}/BF_${SUB}.nii.gz] \
-	    -v 1
-	
-	# Unzip for SUIT (SPM does not work with .gz files)
-	gunzip ${oDIR}/N4_${SUB}.nii.gz
-	
-	# Exit
-	exit
+
+# Logging
+cat <<EOF
+##############################################################
+### Cerebellar Parcelation Pipeline                        ###
+### PART 1: T1 Skull Stripping and Bias Field Correction   ###
+### Start date and time: `date`     ###
+### Subject: ${SID}                                     ###
+### Session: ${SES}                                           ###
+##############################################################
+
+EOF
 
 
-done
 
-	
+# Test for multiple T1 images. If there is more than one T1
+# image, average this image. Otherwise, copy the single image.
+cat <<EOF
+##############################################################
+### Average multiple T1-weighted images (if present)       ###
+##############################################################
+
+EOF
+# Count number of T1 weighted images
+T1list=( $(ls ${iDIR}/sub-${SID}_ses-${SES}_run-?_T1w.nii.gz) )
+echo "Number of T1-weighted images found: ${#T1list[@]}"
+echo "${T1list[@]}" | tr " " "\n" | sed 's/^  *//g'
+
+if [ ${#T1list[@]} -gt 1 ]; then
+
+    # Average T1 weighted images
+    echo "Averaging the ${#T1list[@]} T1-weighted images..."
+    AnatomicalAverage \
+        -v \
+        -n \
+        -o ${oDIR}/T1.nii.gz \
+        ${T1list[@]}
+
+elif [ ${#T1list[@]} -eq 1 ]; then
+
+    # Copy over the single T1 image
+    cp ${T1list[@]} ${oDIR}/T1.nii.gz
+
+elif [ ${#T1list[@]} -eq 0 ]; then
+
+    # No T1 scans found!
+    echo "No T1-weighted images were found. Check your input folder."
+    exit 1
+
+else \
+    # Something went wrong
+    echo "Something went wrong. Please check your code and input arguments."
+    exit 1
+
+fi
+
+
+
+# Skull stripping using ANTs Brain Extraction with the
+# MICCAI2012-Multi-Atlas-Challenge-Data templates.
+cat <<EOF
+
+
+##############################################################
+### Skull strip T1-weighted image using ANTs               ###
+##############################################################
+
+EOF
+
+antsBrainExtraction.sh \
+    -d 3 \
+    -a ${oDIR}/T1.nii.gz \
+    -e ${tDIR}/T_template0.nii.gz \
+    -m ${tDIR}/T_template0_BrainCerebellumProbabilityMask.nii.gz \
+    -f ${tDIR}/T_template0_BrainCerebellumRegistrationMask.nii.gz \
+    -o ${oDIR}/ \
+    -k 0
+
+
+
+# Run N4 Bias Field Correction
+cat <<EOF
+
+
+##############################################################
+### Run N4 Bias Field Correction                           ###
+##############################################################
+
+EOF
+
+# Create a dilated brain mask to make sure we are not excluding
+# any brain tissue, in case the brain mask created by ANTs in
+# the previous step was a bit too tight.
+fslmaths \
+    ${oDIR}/BrainExtractionMask.nii.gz \
+    -dilM -dilM \
+    ${oDIR}/BrainExtractionMask_dilM2.nii.gz
+
+# ITK is very sensitive with any deviations between
+# sform / qform codes beteween images. Even decimal changes
+# will result in problems with the bias field correction.
+# We are therefore going to copy over the affine matrix
+# from the original T1 to the brain masks to avoid this.
+fslcpgeom \
+    ${oDIR}/T1.nii.gz \
+    ${oDIR}/BrainExtractionMask.nii.gz
+
+fslcpgeom \
+    ${oDIR}/T1.nii.gz \
+    ${oDIR}/BrainExtractionMask_dilM2.nii.gz
+
+# Note: adding a mask forces the N4 application within the mask
+# We want to do the estimation within the mask, but the application
+# to the entire image. So we use -w but not -x.
+N4BiasFieldCorrection \
+    -d 3 \
+    -i ${oDIR}/T1.nii.gz \
+    -w ${oDIR}/BrainExtractionMask_dilM2.nii.gz \
+    -s 2 \
+    -c [125x100x75x50] \
+    -o [${oDIR}/N4_T1.nii.gz,${oDIR}/BF_T1.nii.gz] \
+    -v 1
+
+# Exit
+exit
