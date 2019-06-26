9e0e1a5c089f0d004706dfedcb1b60c015e60cdb
Vincent Koppelmans <vincent.koppelmans@utah.edu>
2019-06-26 09:58:21 +0200

    Initialize wrapper script to run Docker

---
+++ runDocker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Wrapper to run the Docker file
+
+usage() {
+    cat <<EOF 
+Run this command as:
+${0} sub-<Your-BIDS-Subject-ID-Goes-Here>
+EOF
+}
+
+# Check input arguments
+if [ -z ${1} ]; then
+    echo "Missing subject argument. Exit."
+    usage
+    exit
+fi
+
+SUBID=${1}
+
+# Run Docker
+docker \
+    run -it \
+    -v /Volumes/lhab_data/LHAB/LHAB_v1.1.1/sourcedata/sub-${SUBID}:/input:ro \
+    -v /Volumes/lhab_data/LHAB/LHAB_v1.1.1/derivatives/SUIT:/output \
+    vkoppelm/suit:lhab \
+    bash /software/scripts/getCerebellarVolumes.sh ${SUBID}
+
+exit
