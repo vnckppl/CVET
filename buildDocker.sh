bb42a846f174fd263695b0eb805d55d38b4950fd
Vincent Koppelmans <vincent.koppelmans@utah.edu>
2019-09-09 10:17:26 -0600

    Made SPM executable for everyone to enable using CVET on the cluster

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 2019-08-31
+# 2019-09-09
 
 # Build Docker image using docker setup file
 DIR="/Users/vincent/Data/Documents/Utah/Kladblok/20171002_Neuroimaging/20190831_CVET/20190521_CVET"
@@ -8,7 +8,7 @@ cd ${DIR}
 
 docker \
     build \
-    -t vkoppelm/cvet:v1.23 \
+    -t vkoppelm/cvet:v1.24 \
     . \
     -f ${DIR}/Dockerfile \
     --cpuset-cpus 3
