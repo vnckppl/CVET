52f0098a38e8828807638b26fafaad6689f6455e
Vincent Koppelmans <vincent.koppelmans@utah.edu>
2020-04-06 13:23:52 -0600

    Include copy FS data inside container + N4 correction.

--- cp2docker/scripts/01_FS.sh
+++ cp2docker/scripts/01_FS.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-# This script runs FreeSurfer
+# * This script runs FreeSurfer
 
-# Input arguments
-while getopts "s:a:c:i:r:" OPTION
+# * Input arguments
+while getopts "s:a:c:i:r:l:n:" OPTION
 do
     case $OPTION in
         s)
-            SID=$OPTARG
+            SID=${OPTARG}
             ;;
         a)
-            AVERAGE=$OPTARG
+            AVERAGE=${OPTARG}
             ;;
         c)
-            CPUS=$OPTARG
+            CPUS=${OPTARG}
             ;;
         i)
-            INTERMEDIATE=$OPTARG
+            INTERMEDIATE=${OPTARG}
             ;;
         r)
-            REPORT=$OPTARG
+            REPORT=${OPTARG}
+            ;;
+        l)
+            LOCALCOPY=${OPTARG}
+            ;;
+        n)
+            N4=${OPTARG}
             ;;
         ?)
         exit
@@ -29,7 +35,7 @@ done
 
 
 
-# Logging
+# * Logging
 cat <<EOF
 ##############################################################
 ### CVET - Cerebellar Volume Extration Tool                ###
@@ -40,13 +46,158 @@ cat <<EOF
 
 EOF
 
-# Environment
+# * Environment
 iDIR=/data/in/sub-${SID}
 oDIR=/data/out/01_FreeSurfer
 mkdir -p ${oDIR}
 
-# List of Sessions
-SESLIST=( $(find ${iDIR} -maxdepth 1 -type d -iname "ses-*" | sort | sed "s#${iDIR}/ses-##g") )
+
+# * Already Processed Data
+# If FreeSurfer data was already processed and
+# the user requests to copy this data to inside the
+# container, do that here.
+if [ ${LOCALCOPY=1} ]; then
+
+    # ** Announce
+    cat <<-EOF
+	##############################################################
+	### Copy processed FreeSurfer data into the container      ###
+	##############################################################
+
+EOF
+
+    echo "Copy Processed FreeSurfer data inside the container"
+
+    # ** Grab subject folders
+    subFolders=(
+        $(find /data/in \
+               -maxdepth 1 \
+               -type d \
+               -iname "sub-${SID}*"
+        )
+    )
+    
+    # ** Copy data
+    # Loop over folders
+    for subFolder in ${subFolders[@]}; do
+        cp -r ${subFolder} ${oDIR}
+    done
+
+    # ** Done
+    # Now we are done with FreeSurfer 'processing',
+    # so we can exit this script.
+    exit
+    
+fi
+
+
+
+# * N4 non-uniformity (bias field) corretion
+# Apply bias field correction on the T1 images
+if [ ${N4} -eq 1 ]; then
+
+    # ** Announce
+    cat <<-EOF
+	##############################################################
+	### Apply N4 biasfield correction to the T1 images         ###
+	##############################################################
+
+EOF
+
+    # ** Environment
+    nDIR=/data/out/00_N4
+    mkdir -p ${nDIR}
+
+    # ** Session list
+    sessions=(
+        $(find {iDIR} \
+               -maxdepth 1 \
+               -type d \
+               -iname "ses-*"
+        )
+    )
+
+    # ** Loop over sessions
+    for session in ${sessions[@]}; do
+
+        # *** Basename
+        SES=$(basename ${session})
+
+        # *** Announce
+        echo "Bias Field Correction of Subject ${SID}, Session ${SES}'"
+
+        # *** Output folder
+        n4oDIR=${nDIR}/${SID}/${SES}/anat
+        mkdir -p ${n4oDIR}
+
+        # *** List all T1 images
+        T1list=(
+            $(find ${iDIR}/${SES}/anat \
+                   -maxdepth 1 \
+                   -type f \
+                   -iname "sub-${SID}_${SES}*T1w.nii*"
+            )
+        )
+        
+        # *** Create brain mask
+        # Loop over T1 images 
+        for T1 in ${T1list[@]}; do
+
+            # **** Basename
+            T1img=$(basename ${T1})
+
+            # **** Naming info
+            # We don't use the entire BIDS naming
+            # scheme, because we use that for globbing
+            # later, and we don't want to mess that up.
+            naming=$(echo ${T1img} \
+                          sed \
+                          -e "s#sub-${SID}_${SES}_##g" \
+                          -e "s#.nii.gz##g" \
+                          -e "s#.nii##g"
+                  )
+
+            # **** Create coarse brain mask
+            # We only need a simple brain mask, because
+            # it is only for directing the esgtimation
+            # of the bias field. It does not need to be
+            # very precise.
+            # Calculate affine registration from MNI to subject
+            flirt \
+                -i ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz \
+                -r ${iDIR}/${SES}/anat/${T1img} \
+                -omat ${n4oDIR}/affine_${naming}.mat
+            # Apply regsitration to MNI brain mask
+            flirt \
+                -i ${FSLDIR}/data/standard/MNI152_T1_1mm_brain_mask.nii.gz \
+                -r ${iDIR}/${SES}/anat/${T1img} \
+                -applyxfm -init ${n4oDIR}/affine_${naming}.mat \
+                -interp nearestneighbour \
+                -o ${n4oDIR}/mask_${naming}.nii.gz
+                
+
+            # **** Run N4 biasfield correction
+            # Run B4 correction with the biasfield estimated
+            # from within the brain mask.
+            N4BiasFieldCorrection \
+                -d 3 \
+                -i ${iDIR}/${SES}/anat/${T1img} \
+                -w ${n4oDIR}/mask_${naming}.nii.gz \
+                -o [ ${n4oDIR}/sub-${SID}_${SES}_N4_${naming}.nii.gz , \
+                     ${n4oDIR}/BF_${naming}.nii.gz ] \
+                -s 2 \
+                -c 100x75x50
+                
+        done
+
+    done
+
+    # ** Set input folder
+    # Now set the input folder to this N4 folder, instead
+    # of the original (mounted) input folder.
+    iDIR=${nDIR}/sub-${SID}
+    
+fi
 
 
 
@@ -59,16 +210,21 @@ cat <<EOF
 
 EOF
 
-# Loop over sessions
+# * List of Sessions
+SESLIST=(
+    $(find ${iDIR} -maxdepth 1 -type d -iname "ses-*" | sort | sed "s#${iDIR}/ses-##g")
+)
+
+# * Loop over sessions
 for SES in ${SESLIST[@]}; do
 
-    # Count number of T1 weighted images
+    # ** Count number of T1 weighted images
     T1list=( $(ls ${iDIR}/ses-${SES}/anat/sub-${SID}_ses-${SES}*T1w.{nii,nii.gz} 2>/dev/null| sort) )
     echo "Number of T1-weighted images found for session ${SES}: ${#T1list[@]}"
     echo "${T1list[@]}" | tr " " "\n" | sed 's/^  *//g'
     echo
 
-    # When the list is empty (no T1 scans found):
+    # ** When the list is empty (no T1 scans found):
     if [ ${#T1list[@]} -eq 0 ]; then
         echo "No T1-weighted images were found for session ${SES}. Check your input folder."
         tmpses=( $(echo ${SESLIST[@]} | sed "s/${SES}//g") )
@@ -76,21 +232,21 @@ for SES in ${SESLIST[@]}; do
         continue
     fi
 
-    # Create variable in which we will store the list of T1 images
+    # ** Create variable in which we will store the list of T1 images
     FS_input_files="$(echo input_${SES})"
     
-    # If selected to average images
+    # ** If selected to average images
     if [ ${AVERAGE} -eq 1 ]; then
 
-        # Check if there is more than one image
+        # *** Check if there is more than one image
         if [ ${#T1list[@]} -gt 1 ]; then
 
-            # Prepare FreeSurfer input command
+            # *** Prepare FreeSurfer input command
             eval ${FS_input_files}='$(echo ${T1list[@]} | sed -e "s/ / -i /g" -e "s/^/-i /g")'
 
         elif [ ${#T1list[@]} -eq 1 ]; then
 
-            # Prepare FreeSurfer input command
+            # *** Prepare FreeSurfer input command
             echo "Only one T1 was found for session ${SES}, even though 'averaging' was selected."
             echo "Continue FreeSurfer with this single T1 image."
             eval ${FS_input_files}='$(echo "-i ${T1list[0]}")'
@@ -107,7 +263,7 @@ done
 
 
 
-# CrossSectional processing with FreeSurfer
+# * CrossSectional processing with FreeSurfer
 cat <<EOF
 
 ##############################################################
@@ -116,20 +272,20 @@ cat <<EOF
 
 EOF
 
-# Loop over sessions
+# * Loop over sessions
 for SES in ${SESLIST[@]}; do
 
-    # Announce
+    # ** Announce
     echo "Working on ${SID}: ${SES} [FreeSurfer CrossSectional Processing]"
 
-    # Pick up input
+    # ** Pick up input
     input="$(echo input_${SES})"
     
-    # Prepare Freesurfer
+    # ** Prepare Freesurfer
     export SUBJECTS_DIR=${oDIR}
     eval "$(echo "recon-all ${!input} -s sub-${SID}_ses-${SES}" | tr "\t" " " | tr "\n" " ")"
     
-    # Run Cross Sectional FreeSurfer
+    # ** Run Cross Sectional FreeSurfer
     recon-all \
         -autorecon-all \
         -parallel \
@@ -138,7 +294,7 @@ for SES in ${SESLIST[@]}; do
     
 done
 
-# Only run the rest of this script if there is more than one time point.
+# * Only run the rest of this script if there is more than one time point.
 if [ ${#SESLIST[@]} -lt 2 ]; then
     echo \
         "Only 1 session found. 
@@ -149,7 +305,7 @@ fi
 
 
 
-# Create Subject Template with FreeSurfer
+# * Create Subject Template with FreeSurfer
 cat <<EOF
 
 
@@ -159,7 +315,7 @@ cat <<EOF
 
 EOF
 
-# Create list of input sessions
+# * Create list of input sessions
 TLIST=(
     $(for SES in ${SESLIST[@]}; do
           echo "-tp $(echo "${SES} " | sed "s/^/sub-${SID}_ses-/g")"
@@ -167,13 +323,13 @@ TLIST=(
     )
 )
 
-# Run template creation
+# * Run template creation
 recon-all -base sub-${SID} ${TLIST[@]} -all -parallel -openmp ${CPUS}
 
 
 
 
-# Longitudinal processing with FreeSurfer
+# * Longitudinal processing with FreeSurfer
 cat <<EOF
 
 ##############################################################
@@ -182,13 +338,13 @@ cat <<EOF
 
 EOF
 
-# Loop over sessions
+# * Loop over sessions
 for SES in ${SESLIST[@]}; do
 
-    # Announce
+    # ** Announce
     echo "Working on ${SID}: ${SES} [FreeSurfer Longitudinal Processing]"
     
-    # Run Longitudinal FreeSurfer
+    # ** Run Longitudinal FreeSurfer
     recon-all \
         -parallel \
         -openmp ${CPUS} \
@@ -200,10 +356,10 @@ done
 
 
 
-# Clean up if flag to keep intermediate files is not set
+# * Clean up if flag to keep intermediate files is not set
 if [ ${INTERMEDIATE} -eq 0 ]; then
     echo "Removing intermediate files has not been implemented yet"
 fi
 
-# Exit
+# * Exit
 exit
